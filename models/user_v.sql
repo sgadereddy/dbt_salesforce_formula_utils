5eba3f1b2a2c7f84b3668804fbdbeb19c937c070
sgadereddy <158325539+sgadereddy@users.noreply.github.com>
2024-01-31 16:51:44 -0500

    Update and rename user_view.sql to user_v.sql

--- models/user_v.sql
+++ models/user_v.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 {{ salesforce_formula_utils.sfdc_formula_view(source_table='user',
     reserved_table_name='user_table',
     full_statement_version = false ) }}
